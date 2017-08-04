d3669ef6ff15ec533713811d9df4092dace60b53
Dre <info@andremcdonald.com>
2017-08-03 22:32:59 -0400

    create a set up form types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormValidationComponent } from './components/form-validation/form-validation.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: FormValidationComponent },
+  { path: 'form-validation', component: FormValidationComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: 'dynamic-form', component: DynamicFormComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormValidationComponent,
+    ReactiveFormComponent,
+    DynamicFormComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
